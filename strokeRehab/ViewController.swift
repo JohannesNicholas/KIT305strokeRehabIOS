767ce54041ae9b3ccdd45c7970879289bf586092
goanna7007 <goanna7007@gmail.com>
2022-05-12 14:16:50 +1000

    Initial Commit

---
+++ strokeRehab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  strokeRehab
+//
+//  Created by mobiledev on 12/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ strokeRehabUITests/strokeRehabUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  strokeRehabUITestsLaunchTests.swift
+//  strokeRehabUITests
+//
+//  Created by mobiledev on 12/5/2022.
+//
+
+import XCTest
+
+class strokeRehabUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
