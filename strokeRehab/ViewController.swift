772d768b4762f711f8d9eef6ae863fd01e9502d1
goanna7007 <goanna7007@gmail.com>
2022-05-12 15:48:36 +1000

    Setup firebase fully

--- strokeRehab/ViewController.swift
+++ strokeRehab/ViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestoreSwift
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        let db = Firestore.firestore()
+        print("\nINITIALIZED FIRESTORE APP \(db.app.name)\n")    }
 
 
 }
