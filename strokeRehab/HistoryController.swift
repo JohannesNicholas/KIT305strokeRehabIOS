d8dea56f88914badc3624a70cfad63321e835885
goanna7007 <goanna7007@gmail.com>
2022-05-16 14:56:07 +1000

    Added basic list to history

---
+++ strokeRehab/HistoryController.swift
@@ -0,0 +1,50 @@
+//
+//  historyController.swift
+//  strokeRehab
+//
+//  Created by mobiledev on 16/5/2022.
+//
+
+import UIKit
+import Firebase
+import FirebaseFirestoreSwift
+
+
+class HistoryController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let db = Firestore.firestore()
+        print("\nINITIALIZED FIRESTORE APP \(db.app.name)\n")    }
+
+
+}
+
+
+extension HistoryController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Youo tapped ", indexPath.row)
+    }
+    
+}
+extension HistoryController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Hello \(indexPath.row)"
+        
+        return cell
+    }
+}
+
