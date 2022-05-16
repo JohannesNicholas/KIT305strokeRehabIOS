d6b5522db25eb98799efe1c0f1c41dd47abf384c
goanna7007 <goanna7007@gmail.com>
2022-05-16 15:15:53 +1000

    Added Record files

---
+++ strokeRehab/Record.swift
@@ -0,0 +1,29 @@
+//
+//  Record.swift
+//  strokeRehab
+//
+//  Created by mobiledev on 16/5/2022.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+public struct Movie : Codable
+{
+    @DocumentID var documentID:String?
+    var buttonsOrNotches: Int32?
+    var goals: Bool?
+    var reps: Int32?
+    var start: Timestamp?
+    var title: String?
+    var messages: [Message]?
+}
+
+public struct Message : Codable
+{
+    var correctPress: Bool?
+    var datetime: Timestamp?
+    var message: String?
+    var rep: Int32?
+}
