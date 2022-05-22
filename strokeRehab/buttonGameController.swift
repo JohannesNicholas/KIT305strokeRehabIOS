b554463b059c3e5ccb4df97656e31d57dff3220a
goanna7007 <goanna7007@gmail.com>
2022-05-22 15:43:51 +1000

    added end button to game screen

---
+++ strokeRehab/buttonGameController.swift
@@ -0,0 +1,34 @@
+//
+//  buttonGameController.swift
+//  strokeRehab
+//
+//  Created by mobiledev on 22/5/2022.
+//
+
+import UIKit
+
+class buttonGameController: UIViewController {
+
+    @IBAction func EndPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ strokeRehab/homeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  homeViewController.swift
+//  strokeRehab
+//
+//  Created by mobiledev on 22/5/2022.
+//
+
+import UIKit
+
+class homeViewController: UIViewController {
+
+    @IBAction func normalGoalsPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "openButtonGame", sender: nil)
+    }
+    
+    @IBAction func sliderGoalsPressed(_ sender: Any) {
+    }
+    
+    @IBAction func normalFreePlayPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func normalSliderPressed(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
