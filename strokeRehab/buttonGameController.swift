bc59592482f1e524dfb25f5fff56c7202070a199
goanna7007 <goanna7007@gmail.com>
2022-05-22 22:34:19 +1000

    copied a bunch of functions over

--- strokeRehab/buttonGameController.swift
+++ strokeRehab/buttonGameController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
+import Firebase
+import FirebaseFirestoreSwift
+
 class buttonGameController: UIViewController {
 
+    @IBOutlet var timeBar: UIProgressView!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
     
     @IBOutlet var buttonA: UIButton!
     @IBOutlet var buttonB: UIButton!
@@ -30,7 +36,21 @@ class buttonGameController: UIViewController {
     @IBOutlet var buttonEHeight: NSLayoutConstraint!
     var buttonSizeConstraints : [NSLayoutConstraint] = []
     
-    
+    @IBAction func buttonApressed(_ sender: Any) {
+        buttonPressed(1)
+    }
+    @IBAction func buttonBpressed(_ sender: Any) {
+        buttonPressed(2)
+    }
+    @IBAction func buttonCpressed(_ sender: Any) {
+        buttonPressed(3)
+    }
+    @IBAction func buttonDpressed(_ sender: Any) {
+        buttonPressed(4)
+    }
+    @IBAction func buttonEpressed(_ sender: Any) {
+        buttonPressed(5)
+    }
     
     
     @IBAction func EndPressed(_ sender: Any) {
@@ -46,7 +66,11 @@ class buttonGameController: UIViewController {
     var timeLimit = 0
     var round = 0
     var nextNumber = 1
+    var time = 0
+    var timer = Timer()
+    var recordData = Record()
     
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -94,9 +118,75 @@ class buttonGameController: UIViewController {
         }
         
         
+        //time limit
+        if (timeLimit != 0) {
+            self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+                
+                
+                
+                if (self.time == 0) {
+                    endOfGame(timeout: true)
+                }
+                
+                self.time -= 1
+                
+                self.timeBar.progress = Float(self.timeLimit - self.time) / Float(self.timeLimit)
+                self.timeLabel.text = String(self.timeLimit - self.time)
+                
+            })
+        }
+        else {
+            timeBar.layer.isHidden = true
+            timeLabel.layer.isHidden = true
+        }
         
         
-        // Do any additional setup after loading the view.
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat="yyyy-MM-dd HH:mm:ss"
+        recordData.documentID = dateFormatter.string(from: Date())
+        recordData.title = "🌐 Normal"
+        recordData.messages = []
+        if freePlay{
+            recordData.reps = nil
+        } else {
+            recordData.reps = Int32(numberOfRounds)
+        }
+        recordData.start = Timestamp.init()
+        recordData.goals = !freePlay
+        
+        newRound()
+        
+        
+        
+        do {
+            try db.collection("Records").document(recordData.documentID ?? "error").setData(from: recordData)
+        } catch let error {
+            print("Error writing record to Firestore: \(error)")
+        }
+    }
+    
+    
+    override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
+        timer.invalidate()
+        super.dismiss(animated: flag, completion: completion)
+    }
+    
+    
+    //called when a button is pressed, passed the number of the button
+    func buttonPressed(number: Int) {
+        record(message: "\(number) Pressed", correctPress: (number == nextNumber))
+        
+        if number == nextNumber {
+            
+            if (nextNumber == numberOfButtons) {
+                newRound()
+            }
+            else {
+                nextNumber += 1
+            }
+         
+            highlightNextNum()
+        }
     }
     
 
